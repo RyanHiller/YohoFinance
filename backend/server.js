b159a8dd5e958c6ccfa486ad03e75752b232ab37
Ryan Hiller <allamericanslacker@gmail.com>
2021-04-13 15:33:37 -0600

    Rename index.js to server.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,19 @@
 const express = require('express');
+const yf = require('yahoo-finance');
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+
+
+
+
+
+yf.quote({
+  symbol: '^DJI',
+  modules: ['price']
+}, (err, quote) => {
+  err ? console.log(err) : console.log(quote);
+});
