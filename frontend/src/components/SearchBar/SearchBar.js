2527aab70fde013a8179a49ff8f3d24b4c2e139a
Ryan Hiller <allamericanslacker@gmail.com>
2021-04-14 19:25:23 -0600

    Search handler 1.0

--- frontend/src/components/SearchBar/SearchBar.js
+++ frontend/src/components/SearchBar/SearchBar.js
@@ -1,18 +1,40 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 import styles from './SearchBar.module.css';
 
-export default function SearchBar() {
+export default function SearchBar(props) {
+  const [searchTerm, setSearchTerm] = React.useState('');
+  const history = useHistory();
+
+  function handleChange(event) {
+    setSearchTerm(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    if (!(searchTerm.trim().length > 0)) {
+      event.preventDefault();
+      return;
+    } else {
+      event.preventDefault();
+      const inputValue = searchTerm.trim().toUpperCase();
+      history.push(`/quote/${inputValue}`);
+    }
+  }
+
   return (
-    <form className={styles.SearchBar}>
+    <form className={styles.SearchBar} onSubmit={handleSubmit}>
       <label>Search</label>
       <input
         aria-label='Search for news, symbols or companies'
-        placeholder='Search for news, symbols or companies'
         autoCapitalize='none'
         autoComplete='off'
+        onChange={handleChange}
+        placeholder='Search for news, symbols or companies'
+        type='text'
+        value={searchTerm}
       />
-      <button type="submit">O</button>
+      <button type='submit'>O</button>
     </form>
   );
 }
