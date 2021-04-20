813d0b5d4e9401781cb9d876ac12ba7659b4f2aa
Ryan Hiller <allamericanslacker@gmail.com>
2021-04-20 11:37:43 -0600

    More work on single ticker page

--- frontend/src/components/QuoteHeader/QuoteHeader.js
+++ frontend/src/components/QuoteHeader/QuoteHeader.js
@@ -3,7 +3,15 @@ import React, { useEffect } from 'react';
 import styles from './QuoteHeader.module.css';
 
 export default function QuoteHeader(props) {
-  let rmChange, rmColor, rmPercent, rmPrice, rmSign, currentMarket;
+  let rmChange,
+    rmColor,
+    rmPercent,
+    rmPrice,
+    rmSign,
+    rmTimeNotice,
+    currentMarket;
+
+  const [rmStatus, setRMStatus] = React.useState('closed');
 
   React.useEffect(determineMarket, []);
 
@@ -27,17 +35,28 @@ export default function QuoteHeader(props) {
     rmPrice = props.ticker.regularMarketPrice.toFixed(4);
   }
 
+  // Format RM Notice clock
+  rmTimeNotice = new Date(props.ticker.regularMarketTime)
+  rmTimeNotice = rmTimeNotice.toLocaleString('en-US', { timeZone: 'America/New_York' });
+  rmTimeNotice = rmTimeNotice.split(' ');
+  rmTimeNotice.shift();
+  rmTimeNotice[0] = rmTimeNotice[0].slice(0, -3);
+  rmTimeNotice = rmTimeNotice.join('') + ' EDT';
+
   // Determine Premarket/Regular Market/After Hours
   function determineMarket() {
     let currentTime = new Date();
     currentTime.setMilliseconds(0);
     currentTime.setSeconds(0);
+
+    //This is a for loop so we can use break
     for (let x = 1; x > 0; ) {
       let pmTime = new Date(props.ticker.preMarketTime);
       pmTime.setMilliseconds(0);
       pmTime.setSeconds(0);
       if (currentTime.getTime() === pmTime.getTime()) {
         currentMarket = 'PM';
+        setRMStatus('closed');
         break;
       }
 
@@ -46,6 +65,7 @@ export default function QuoteHeader(props) {
       rmTime.setSeconds(0);
       if (currentTime.getTime() === rmTime.getTime()) {
         currentMarket = 'RM';
+        setRMStatus('open');
         break;
       }
 
@@ -54,14 +74,14 @@ export default function QuoteHeader(props) {
       ahTime.setSeconds(0);
       if (currentTime.getTime() === ahTime.getTime()) {
         currentMarket = 'AH';
+        setRMStatus('closed');
         break;
       }
 
       currentMarket = 'CL';
+      setRMStatus('closed');
       break;
     }
-
-    console.log(currentMarket);
   }
 
   return (
@@ -79,14 +99,16 @@ export default function QuoteHeader(props) {
       <div className={styles.Pricing}>
         <div className={styles.RegularMarket}>
           <div className={styles.RMPriceContainer}>
-            <div className={styles.RMPrice}>{rmPrice}</div>
-            <div className={`${styles.RMChange} ${rmColor}`}>
-              {rmSign}
-              {rmChange}
+            <div className={styles.RMPrice}>
+              {rmPrice}{' '}
+              <span className={`${styles.RMChange} ${rmColor}`}>
+                {rmSign}
+                {rmChange} ({rmSign}
+                {rmPercent}%)
+              </span>
             </div>
-            <div className={`${styles.RMChange} ${rmColor}`}>
-              ({rmSign}
-              {rmPercent}%)
+            <div className={styles.RMNotice}>
+              As of {new Date(props.ticker.regularMarketTime).toString()}
             </div>
           </div>
           <div className={styles.RMNotice}></div>
